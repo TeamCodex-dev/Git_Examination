7d93297e1b2ddfc573bb1607c62ec39d7d7397d6
NeranjayaDilshan <neranjayadilshan@gmail.com>
2021-10-09 10:19:56 +0530

    Create Customer UI

---
+++ src/controller/CustomerController.java
@@ -0,0 +1,58 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class CustomerController {
+
+    @FXML
+    private JFXTextField txtCustId;
+
+    @FXML
+    private JFXTextField txtNic;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtContact;
+
+    @FXML
+    private JFXButton btnSave;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    void custIDOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tctContactOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAddressOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNicOnAction(ActionEvent event) {
+
+    }
+
+}
